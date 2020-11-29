55828cca9c4b525679efa50c079c6e0c2b7e3cac
Paulo Gustavo Araujo Santana <54332693+Ollke@users.noreply.github.com>
2020-11-28 21:29:13 -0300

    extraindo dados e criando o grafo de usarios

---
+++ temp.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+DataMovies = pd.read_csv("ml-latest-small/movies.csv", sep=",")
+
+MoviesTitle = DataMovies["title"]
+MoviesId = DataMovies["movieId"]
+MoviesGenres = DataMovies["genres"]
+aux = []
+
+def extract(x):
+    aux = []
+    for i in x:
+        aux.append(i)
+    return aux
+
+MoviesTitle = extract(MoviesTitle)
+MoviesId = extract(MoviesId)
+MoviesGenres = extract(MoviesGenres)
+
+Movies = {}
+recommended = []
+
+for i in range(len(MoviesTitle)):
+    Movies[MoviesId[i]] = [MoviesTitle[i], MoviesGenres[i].split("|"), 6]
+
+
+
+DataRatings = pd.read_csv("ml-latest-small/ratings.csv", sep=",")
+
+RatingsUserId = DataRatings["userId"]
+RatingsMoviesId = DataRatings["movieId"]
+RatingsList = DataRatings["rating"]
+
+RatingsUserId = extract(RatingsUserId)
+RatingsMoviesId = extract(RatingsMoviesId)
+RatingsList = extract(RatingsList)
+
+Ratings = {}
+users = {}
+
+for i in RatingsUserId:
+    Ratings[i] = []
+    users[i] = []
+
+for i in range(len(RatingsUserId)):
+    Ratings[RatingsUserId[i]].append([RatingsMoviesId[i],RatingsList[i]])
+
+adj = int(input("Digite o numero de conexoes que cada usuario pode ter (recomendado 50):"))
+
+for i in Ratings.keys():
+    for j in Ratings.keys():
+        if len(users[i])>adj:
+            break
+
+        if i != j:
+            for k in Ratings[i]:
+                if j not in users[i]:
+                        for z in Ratings[j]:
+                            if k[0] == z[0] and z[1] == k[1]:
+                                users[i].append(j)
+                                break
+
